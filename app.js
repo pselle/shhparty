ec14751c44f1b3904e89fad8253bfda96e94d6b3
pselle <pamela.selle@gmail.com>
2015-04-17 17:51:05 -0400

    Basic express setup, can tell you its localip yo

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
+var express = require('express')
 var sys = require('sys')
-var exec = require('child_process').exec;
+var exec = require('child_process').exec
 
-function getLocalIp() {
-  exec("ifconfig | grep 'inet ' | grep -v 127.0.0.1",
+// Sets local IP in a totally non-elegant way
+var localIP
+(function getLocalIp() {
+  var local = exec("ifconfig | grep 'inet ' | grep -v 127.0.0.1",
    function parseIP(error, stdout, stderr) {
       var re = /inet (\d*\S*) netmask/
-      var localIP = re.exec(stdout)[1]
+      var parsedIP = re.exec(stdout)[1]
+      localIP = parsedIP
       console.log(localIP)
     }
   );
-}
+})()
 
-getLocalIp()
+var app = express();
+
+app.use(express.static('public'));
+// view engine setup
+app.set('view engine', 'jade');
+
+app.get('/', function (req, res) {
+  res.render('index', { title: localIP });
+});
+
+var server = app.listen(6799, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
+
