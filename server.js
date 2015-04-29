da0d0da9f61e44499c9982f737bcf2f6da1bf01f
pselle <pamela.selle@gmail.com>
2015-04-29 16:07:48 -0400

    Obnoxious whitespace commits

--- server.js
+++ server.js
@@ -15,41 +15,41 @@ var runningPortNumber = process.env.PORT;
 
 
 app.configure(function(){
-	// I need to access everything in '/public' directly
-	app.use(express.static(__dirname + '/public'));
+  // I need to access everything in '/public' directly
+  app.use(express.static(__dirname + '/public'));
 
-	//set the view engine
-	app.set('view engine', 'ejs');
-	app.set('views', __dirname +'/views');
+  //set the view engine
+  app.set('view engine', 'ejs');
+  app.set('views', __dirname +'/views');
 
-	app.use(device.capture());
+  app.use(device.capture());
 });
 
 
 // logs every request
 app.use(function(req, res, next){
-	// output every request in the array
-	console.log({method:req.method, url: req.url, device: req.device});
+  // output every request in the array
+  console.log({method:req.method, url: req.url, device: req.device});
 
-	// goes onto the next function in line
-	next();
+  // goes onto the next function in line
+  next();
 });
 
 app.get("/", function(req, res){
-	res.render('index', {});
+  res.render('index', {});
 });
 
 
 io.sockets.on('connection', function (socket) {
 
-	io.sockets.emit('blast', {msg:"<span style=\"color:red !important\">someone connected</span>"});
+  io.sockets.emit('blast', {msg:"<span style=\"color:red !important\">someone connected</span>"});
 
-	socket.on('blast', function(data, fn){
-		console.log(data);
-		io.sockets.emit('blast', {msg:data.msg});
+  socket.on('blast', function(data, fn){
+    console.log(data);
+    io.sockets.emit('blast', {msg:data.msg});
 
-		fn();//call the client back to clear out the field
-	});
+    fn();//call the client back to clear out the field
+  });
 
 });
 
