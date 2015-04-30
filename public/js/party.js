9255167c04bc8caa9259d0be9953d765c56f82b8
Nat Welch <nat@natwelch.com>
2015-04-30 21:19:20 +0000

    Merge!

--- public/js/party.js
+++ public/js/party.js
@@ -1,13 +1,18 @@
 var socket = io.connect('/')
 var peer = new Peer({key: 'fr9d131o9wwmi'})
 
+// Open a connection
 peer.on('connection', function(conn) {
+  // On recieve data, do stuff.
   conn.on('data', function(data){
     console.log(data);
   });
 });
 
+// Ok, you're not the DJ. So, you first need to tell the DJ that you are here,
+// and you are ready to party.
 var conn = peer.connect(dj)
 conn.on('open', function() {
-  conn.send({ id: peer.id })
+  conn.send({ type: "here", party: dj, id: peer.id })
+  console.log("Said I'm here.")
 })
