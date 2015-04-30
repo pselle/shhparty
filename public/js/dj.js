ed4b02d8228161cde7838d49764caa3ba17fb145
Nat Welch <nat@natwelch.com>
2015-04-30 21:19:20 +0000

    Merge!

--- public/js/dj.js
+++ public/js/dj.js
@@ -1,9 +1,27 @@
-var socket = io.connect('/')
-// Get DJ id from browser
-var peer = new Peer(window.location.pathname.split('/')[2], {key: 'fr9d131o9wwmi'})
 
+var socket = io.connect('/')
+
+// The dj var is written by the server side template.
+console.log("You are the dj:", dj)
+var peer = new Peer(dj, {key: 'fr9d131o9wwmi'})
+var partiers = []
+
+// Open connection to recieve data.
 peer.on('connection', function(conn) {
-  conn.on('data', function(data){
+  conn.on('data', function(data) {
     console.log(data);
+
+    // Someone is here to party, check to make sure they are at the right
+    // party, add them to the list of people partying.
+    if (data.type != undefined && data.party != undefined && data.id != undefined) {
+      if (data.type == "here" && data.party == dj) {
+        partiers.push(data.id)
+        console.log("New Partier", data.id)
+      } else {
+        console.log("Person trying to get to wrong party.")
+      }
+
+      console.log("Partier Count", partiers.length)
+    }
   });
 });
