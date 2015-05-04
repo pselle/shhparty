557aa5b82b4e5b6c1246da2a150150e7c9fe9489
Nat Welch <nat@natwelch.com>
2015-05-04 20:02:03 +0000

    Store Audio file grabbing idea

--- public/js/dj.js
+++ public/js/dj.js
@@ -5,8 +5,8 @@ console.log("You are the dj:", dj)
 var peer = new Peer(dj, {key: 'fr9d131o9wwmi'})
 var partiers = []
 
-var stream;
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+var stream
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia
 
 if (navigator.getUserMedia) {
   navigator.getUserMedia(
@@ -15,16 +15,16 @@ if (navigator.getUserMedia) {
         stream = incoming
       },
       function(err) {
-        console.log("The following error occured: " + err.name);
-      });
+        console.log("The following error occured: " + err.name)
+      })
 } else {
-  console.error("getUserMedia not supported");
+  console.error("getUserMedia not supported")
 }
 
 // Open connection to recieve data.
 peer.on('connection', function(conn) {
   conn.on('data', function(data) {
-    console.log(data);
+    console.log(data)
 
     // Someone is here to party, check to make sure they are at the right
     // party, add them to the list of people partying.
@@ -39,11 +39,34 @@ peer.on('connection', function(conn) {
 
       console.log("Partier Count", partiers.length)
     }
-  });
-});
+  })
+})
 
 // When the DJ closes their window, let the server know they're leaving
 window.onbeforeunload = function() {
   socket.emit("closedParty", { peerId: peer.id })
   return null
+}
+
+// A function to load a file, doesn't work yet.
+function LoadFile() {
+  var file = document.querySelector('input[type=file]').files[0]
+  var reader = new FileReader()
+  var context = new AudioContext();
+
+  // Why do this? I has no idea.
+  var gainNode = context.createGain();
+  gainNode.connect(context.destination);
+
+  reader.onload = (function(e) {
+    // Import callback function that provides PCM audio data decoded as an
+    // audio buffer.
+    context.decodeAudioData(e.target.result, function(buffer) {
+      var destination = context.createMediaStreamDestination()
+      // TODO: This doesn't work, because browsers haven't implemented ways to
+      // swap out tracks yet.
+      // stream.addTrack(destination.stream.getAudioTracks()[0])
+    })
+  })
+  reader.readAsArrayBuffer(file);
 }
